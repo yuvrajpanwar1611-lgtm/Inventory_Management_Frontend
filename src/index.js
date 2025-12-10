8865eeca3bed0fa35a4b5e1d852c89c9594a1284
yuvrajpanwar1611-lgtm <yuvrajpanwar1611@gmail.com>
2025-12-10 16:18:57 +0530

    changed code acc. to static email otp

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useContext } from "react";
-import { AuthContext } from "./AuthContext";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const { token } = useContext(AuthContext);
-const isLoggedIn = Boolean(token);
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);

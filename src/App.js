6c52b123490b0364a5ad45c34ff24a90a8b96f0e
yuvrajpanwar1611-lgtm <yuvrajpanwar1611@gmail.com>
2025-12-15 14:37:11 +0530

    cds

--- src/App.js
+++ src/App.js
@@ -1,129 +1,4 @@
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 
-// import NavBar from "./components/NavBar";
-// import ProductsTable from "./components/ProductsTable";
-// import AddProducts from "./components/AddProducts";
-// import UpdateProduct from "./components/UpdateProduct";
-// import SupplierPage from "./components/SupplierPage";
-// import PurchaseProduct from "./components/PurchaseProduct";
-// import SellProduct from "./components/SellProduct";
-// import StockMovement from "./components/StockMovement";
-// import ChatBot from "./components/ChatBot";
-
-// import Login from "./Login";
-// import Signup from "./Signup";
-// import ProtectedRoute from "./ProtectedRoute";
-
-// import { ProductProvider } from "./ProductContext";
-// import { SupplierContextProvider } from "./SupplierContext";
-// import { UpdateProductContextProvider } from "./UpdateProductContext";
-
-// function Layout() {
-//   const location = useLocation();
-//   const isLoggedIn = Boolean(localStorage.getItem("token"));
-
-//   // ❌ Hide NavBar & ChatBot on Login + Signup pages
-//   const hideUI =
-//     location.pathname === "/login" || location.pathname === "/signup";
-
-//   return (
-//     <>
-//       {/* NAVBAR → Show only when logged in */}
-//       {!hideUI && isLoggedIn && <NavBar />}
-
-//       <Routes>
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/signup" element={<Signup />} />
-
-//         <Route
-//           path="/"
-//           element={
-//             <ProtectedRoute>
-//               <ProductsTable />
-//             </ProtectedRoute>
-//           }
-//         />
-
-//         <Route
-//           path="/addproduct"
-//           element={
-//             <ProtectedRoute>
-//               <AddProducts />
-//             </ProtectedRoute>
-//           }
-//         />
-
-//         <Route
-//           path="/updateproduct"
-//           element={
-//             <ProtectedRoute>
-//               <UpdateProduct />
-//             </ProtectedRoute>
-//           }
-//         />
-
-//         <Route
-//           path="/purchase"
-//           element={
-//             <ProtectedRoute>
-//               <PurchaseProduct />
-//             </ProtectedRoute>
-//           }
-//         />
-
-//         <Route
-//           path="/sell"
-//           element={
-//             <ProtectedRoute>
-//               <SellProduct />
-//             </ProtectedRoute>
-//           }
-//         />
-
-//         <Route
-//           path="/suppliers"
-//           element={
-//             <ProtectedRoute>
-//               <SupplierPage />
-//             </ProtectedRoute>
-//           }
-//         />
-
-//         <Route
-//           path="/movement/:id"
-//           element={
-//             <ProtectedRoute>
-//               <StockMovement />
-//             </ProtectedRoute>
-//           }
-//         />
-//       </Routes>
-
-//       {/* CHATBOT → Show only when logged in AND not on login/signup */}
-//       {!hideUI && isLoggedIn && <ChatBot />}
-//     </>
-//   );
-// }
-
-// function App() {
-//   return (
-//      <AuthProvider>
-//       <ProductProvider>
-//         <SupplierContextProvider>
-//           <UpdateProductContextProvider>
-//             <Router>
-//               <Layout />
-//             </Router>
-//           </UpdateProductContextProvider>
-//         </SupplierContextProvider>
-//       </ProductProvider>
-//     </AuthProvider>
-//   );
-// }
-
-// src/App.js
-// src/App.js
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -156,11 +31,12 @@ import { UpdateProductContextProvider } from "./UpdateProductContext";
 
 function Layout() {
   const location = useLocation();
-  const { token } = React.useContext(AuthContext);
+  const auth = React.useContext(AuthContext);
+  const token = auth?.token;
 
   // Hide Navbar & ChatBot ONLY on Login + Signup
   const hideUI = location.pathname === "/login" || location.pathname === "/signup";
-
+  
   return (
     <>
       {/* Navbar visible when authenticated and not on login/signup */}
@@ -251,15 +127,15 @@ function App() {
       <ProductProvider>
         <SupplierContextProvider>
           <UpdateProductContextProvider>
-            <Router>
-              <Layout />
-            </Router>
+           <Router>
+             <Layout />
+           </Router>
           </UpdateProductContextProvider>
         </SupplierContextProvider>
       </ProductProvider>
     </AuthProvider>
-  );
-}
+      );
+    }
 
 export default App;
 
